4f59fb01d55d6a4eef33cff8615f9c97bd1aead6
Memet Ekici <meban2009@gmail.com>
2021-11-12 17:22:58 +0100

    Benim ilk proje yollamam

---
+++ src/Bölüm03/package-info.java
@@ -0,0 +1,1 @@
+package Bölüm03;
---
+++ src/bölüm02/AlanHesaplama.java
@@ -0,0 +1,21 @@
+package bölüm02;
+
+public class AlanHesaplama {
+	public static void main(String[]args) {
+		
+		double yarýcap;
+		double alan;
+		
+		//yarýcapý oku
+		yarýcap = 20;
+		
+		//alaný hesapla
+		alan = yarýcap * yarýcap *3.14159;
+		
+		//alaný yazdýr
+		System.out.println("yarýcapý " + yarýcap + " olan çemberin alaný:" + alan + " dýr");
+		
+
+	}
+
+}
