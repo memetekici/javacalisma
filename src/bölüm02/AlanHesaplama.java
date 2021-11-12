9df69cc26681330d87217d3324e598414de41ff4
Memet Ekici <meban2009@gmail.com>
2021-11-12 17:44:47 +0100

    kendi bilgisayarımddan 2 ci commitim

--- src/bölüm02/AlanHesaplama.java
+++ src/bölüm02/AlanHesaplama.java
@@ -16,6 +16,7 @@ public class AlanHesaplama {
 		System.out.println("yarýcapý " + yarýcap + " olan çemberin alaný:" + alan + " dýr");
 		
 		System.out.println("Gitub çalýþmam için yolladýðým ilk deðiþim");
+		System.out.println("Gitub çalýþmam için yolladýðým 2. deðiþim");
 		
 
 	}
